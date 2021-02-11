c68299b674ab4b14bc3c00b24e425094a129ed15
rayleighko <ray@scentlab.kr>
2021-02-12 00:22:40 +0900

    fix: utils index file

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,3 @@
-import GoogleAnalytics from './GoogleAnalytics/GoogleAnalytics.svelte'
-import * as ga from './GoogleAnalytics/generated-functions.js'
-
-export { GoogleAnalytics, ga }
+export { default as GoogleAnalytics } from './GoogleAnalytics/GoogleAnalytics.svelte'
+export * as ga from './GoogleAnalytics/generated-functions.js'
+export * as PC from './PerfumeCalc/PerfumeCalc'
