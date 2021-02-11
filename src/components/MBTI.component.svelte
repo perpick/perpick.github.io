2019d8504c98f7d3c9a9604652a5106db17b6d68
rayleighko <ray@scentlab.kr>
2021-02-12 00:15:30 +0900

    feat: Creating the required component frame

---
+++ src/components/MBTI.component.svelte
@@ -0,0 +1,18 @@
+<script lang="typescript">
+  export let descriptions: {
+    mbti: string;
+    sub_title: string;
+    advice: string;
+    title: string;
+    detail: string;
+    word: string;
+    animal: string;
+  }
+  export let tags: string[]
+</script>
+  
+<style>
+</style>
+    
+<div class="">
+</div>
---
+++ src/components/Quiz.component.svelte
@@ -0,0 +1,26 @@
+<script lang="typescript">
+  import { users, pagination } from "@src/stores";
+
+  export let quiz: {
+    title: string;
+    selectors: {
+      title: string;
+      data: string;
+    }[]
+  };
+
+  const handleClickSelct = (data: string) => {
+    console.log(data)
+  }
+</script>
+
+<style>
+</style>
+  
+<div class="">
+  {#each quiz.selectors as selector}
+  <button on:click={() => handleClickSelct(selector.data)}>{selector.title}</button>
+	{/each}
+
+  <p>now : {$pagination.page} / {$pagination.max}</p>
+</div>
