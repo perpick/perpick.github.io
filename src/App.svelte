6b08af4c6c8897b724728b8a2c106554e4908b2b
rayleighko <ray@scentlab.kr>
2021-02-12 00:20:41 +0900

    fix: modify to basic component frame

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,30 @@
 <script lang="typescript">
+  import { nav } from './stores'
+	import Home from "./pages/Home.page.svelte";
+	import Result from "./pages/Result.page.svelte";
+	import Quiz from "./pages/Quiz.page.svelte";
+
   import { GoogleAnalytics } from "@src/utils";
 
   import "./styles/TailwindStyles.svelte";
-
-  const message = "Learn Svelte";
+  
 </script>
 
-<GoogleAnalytics mainTagManagerId={"1"} subTagManagerId={[""]} />
-<div class="text-center font-serif">
-  <header
-    class="bg-gray-100 h-screen flex justify-center items-center flex-col text-3xl"
-  >
-    <img
-      src="/logo.svg"
-      class="App-logo pointer-events-none m-4 h-64"
-      alt="logo"
-    />
-    <p>
-      Edit <code>src/App.svelte</code> and save to reload.
-    </p>
-    <a
-      class="text-orange-500"
-      href="https://svelte.dev"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      {message}
-    </a>
-  </header>
-</div>
-
 <style>
-  .App-logo {
-    animation: App-logo-scale infinite 1.6s ease-in-out alternate;
-  }
-  @keyframes App-logo-scale {
-    from {
-      transform: scale(1);
-    }
-    to {
-      transform: scale(1.06);
-    }
-  }
+  
 </style>
+
+<GoogleAnalytics />
+<header>
+</header>
+<div>
+  {#if $nav.current === 'home'}
+    <Home />
+  {/if}
+  {#if $nav.current === 'result'}
+    <Result />
+  {/if}
+  {#if $nav.current === 'quizzes'}
+    <Quiz />
+  {/if}
+</div>
