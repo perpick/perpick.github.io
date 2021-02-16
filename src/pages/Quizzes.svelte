0a79e48feb66a92ce455159bdf932ac8cef2ed01
rayleighko <ray@scentlab.kr>
2021-02-16 16:39:10 +0900

    fix: styles

--- src/pages/Quizzes.svelte
+++ src/pages/Quizzes.svelte
@@ -1,19 +1,25 @@
 <script lang="ts">
-    import Quiz from '@src/components/Quiz/Quiz.svelte'
-    import { pagination } from "@src/stores";
-    import quizzes from '@src/constants/quizzes.json'
-
+  import Quiz from '@src/components/Quiz/Quiz.svelte'
+  import { pagination } from "@src/stores";
+  import quizzes from '@src/constants/quizzes.json'
 </script>
-  
-<style>
-</style>
 
-<div class="relative pt-1">
+<div class="relative pt-1 m-4">
   <div class="flex mb-2 items-center justify-between">
     <div>
-      <span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200">
-        나에게 꼭 맞는 향수 찾아가는 중
+      {#if Math.ceil(($pagination.page / ($pagination.max + 1)) *100) < 33}
+      <span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-green-500 bg-pg-200">
+        이게 향수 추천과 관련 있는지 궁금해 하는 중
       </span>
+      {:else if Math.ceil(($pagination.page / ($pagination.max + 1)) *100) < 66}
+      <span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-purple-500 bg-pb-200">
+        이제 그만할까 생각하는 중
+      </span>
+      {:else}
+      <span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-500 bg-pp-200">
+        거의 다 왔어! 힘을 내라구!
+      </span>
+      {/if}
     </div>
     <div class="text-right">
       <span class="text-xs font-semibold inline-block text-pink-600">
