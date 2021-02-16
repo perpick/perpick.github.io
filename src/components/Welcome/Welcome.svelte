6d99a40d974fcc3cbd4f3151c320eebc0d0825ac
rayleighko <ray@scentlab.kr>
2021-02-16 14:45:33 +0900

    fix: modify welcome text

--- src/components/Welcome/Welcome.svelte
+++ src/components/Welcome/Welcome.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 	import { nav } from '@src/stores'
     import Typewriter from '@src/components/Typewriter/Typewriter.svelte'
-	import TooltipBtn from '@src/components/Commons/TooltipBtn.svelte'
-	// import { tooltip } from '@src/utils/tooltip'
-	
+
 	const handleClickNext = () => {
 		$nav.current = 'quizzes'
 	}
@@ -19,5 +17,5 @@
 	<button class="bg-indigo-400 hover:bg-indigo-500 text-white font-bold py-2 px-4 rounded" on:click={handleClickNext} >나만의 향수 찾으러 가기</button>
 </div>
 <Typewriter interval={200}>
-	<h1>현재까지 300명이 함께 하고 있어요!</h1>
+	<h1>현재까지 1,329명이 함께 하고 있어요!</h1>
 </Typewriter>
