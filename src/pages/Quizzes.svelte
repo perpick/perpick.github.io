369bea1c0b163c554a5552e2d550a81d27b342ea
rayleighko <ray@scentlab.kr>
2021-02-16 14:45:19 +0900

    feat: add progress bar

--- src/pages/Quizzes.svelte
+++ src/pages/Quizzes.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import Quiz from '@src/components/Quiz/Quiz.svelte'
+    import { pagination } from "@src/stores";
     import quizzes from '@src/constants/quizzes.json'
+
 </script>
   
 <style>
@@ -8,7 +10,7 @@
 
 <div class="relative pt-1">
     <div class="overflow-hidden h-2 mb-4 text-xs flex rounded bg-pink-200">
-      <div style="width:30%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-pink-500"></div>
+      <div style={`width:${Math.ceil(($pagination.page / ($pagination.max + 1)) *100)}%`} class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-pink-500"></div>
     </div>
   </div>
 {#each quizzes as quiz}
