ec4770e2f58f725677e93a68b4239f47dde46c6c
rayleighko <ray@scentlab.kr>
2021-02-17 18:28:48 +0900

    fix: placehold

--- src/components/MailChimp.svelte
+++ src/components/MailChimp.svelte
@@ -11,13 +11,13 @@
     <form action="https://perpick.us7.list-manage.com/subscribe/post?u=ab790a36cc4ef5106067ad42d&amp;id=2066ae7171" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
         <div id="mc_embed_signup_scroll">
             <label for="mce-EMAIL" class="text-gray-500">하단의 구독을 통해 퍼픽의 최신 소식을 만나세요!</label>
-            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="이메일을 입력해주세요!" required>
             <div style="position: absolute; left: -5000px;" aria-hidden="true">
                 <input type="text" name="b_ab790a36cc4ef5106067ad42d_2066ae7171" tabindex="-1" value="">
             </div>
         </div>
         <div class="clear mt-4">
-            <input type="submit" value="함께하기" name="subscribe" id="mc-embedded-subscribe" class="py-2 px-4 text-pp-50 bg-pp-800 hover:bg-pp-50 hover:text-pp-800 active:bg-pp-50 active:bg-text-pp-800 rounded">
+            <input type="submit" value="함께하기" name="subscribe" id="mc-embedded-subscribe" class="py-2 px-4 text-pp-50 bg-pp-800 hover:bg-pp-50 hover:text-pp-800 active:bg-pp-50 active:bg-text-pp-800 rounded cursor-pointer">
         </div>
     </form>
 </div>
