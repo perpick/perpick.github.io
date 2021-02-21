8c1da1b813554b4faf7adfa9577ced0e1668564c
rayleighko <ray@scentlab.kr>
2021-02-21 22:26:12 +0900

    fix: page - all perpick types

--- src/pages/Types.svelte
+++ src/pages/Types.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
     import { nav, users } from "@src/stores";
+    import IntersectionObserver from '@src/components/Image/IntersectionObserver.svelte'
 
     import mbtis from '@src/constants/mbtis.json'
+    import titles from '@src/constants/titles.json'
+    import gender_tags from '@src/constants/gender_tags.json'
 
     let gender: string = 'FEMALE'
-    let title: string = 'sunrise'
+    let title: string = 'city'
 
     const handleClickMBTI = (mbti: { mbti: string; sub_title: string; advice: string; title: string; detail: string; word: string; animal: string; best: string; best_title: string; }) => {
-        $users.selects = [...mbti.mbti.split(''), gender, title]
+        $users.selects = [gender, title, ...mbti.mbti.split(''),]
         
         $nav.current = "type"
     }
@@ -20,54 +23,56 @@
     }
 </style>
 
-<div class="pt-4 bg-white mb-4 shadow-lg m-4 rounded">
-    <div class="flex flex-wrap">
-        <div class="w-full">
-            <div class="px-1">
-                <h2>성별</h2>
-                <ul class="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row  justify-center items-center">
-                    {#each ['FEMALE', 'MALE'] as select}
-                        <li class="-mb-px mx-1 my-2 last:mr-0 flex-auto text-center">
-                            <p 
-                                on:click={() => {
-                                    gender = select
-                                }}
-                                class={`cursor-pointer text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal text-white ${gender === select ? "bg-pb-800" : "bg-gray-300"}`} >
-                                <i class="fas fa-space-shuttle text-base mr-1"></i>  { select }
-                            </p>
-                        </li>
-                    {/each}
-                </ul>
-            <div class="px-1">
-            </div>
-                <h2>타이틀</h2>
-                <ul class="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row  justify-center items-center">
-                    {#each ['sunrise', 'sunset', 'city', 'nature'] as select}
-                        <li class="-mb-px mx-1  my-2 last:mr-0 flex-auto text-center">
-                            <p 
-                                on:click={() => {
-                                    title = select
-                                }}
-                                class={`cursor-pointer text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal text-white  ${title === select ? "bg-pb-800" : "bg-gray-300"}`} >
-                                <i class="fas fa-space-shuttle text-base mr-1"></i>  { select }
-                            </p>
-                        </li>
-                    {/each}
-                </ul>
-            </div>
-            <div class="px-1">
-                <h2>동물</h2>
-                <ul class="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row  justify-center items-center">
-                    {#each mbtis as mbti}
-                        <li class="-mb-px mx-1 my-2 last:mr-0 flex-auto text-center" on:click={() => handleClickMBTI(mbti)}>
-                            <p 
-                                class={`cursor-pointer text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal text-white  bg-purple-500`} >
-                                <i class="fas fa-space-shuttle text-base mr-1"></i>  { mbti.animal }
-                            </p>
-                        </li>
-                    {/each}
-                </ul>
+<IntersectionObserver>
+    <div class="pt-4 bg-white mb-4 shadow-lg m-4 rounded">
+        <div class="flex flex-wrap">
+            <div class="w-full">
+                <div class="px-1 m-4">
+                    <h2>성별</h2>
+                    <ul class="grid grid-cols-2 gap-2">
+                        {#each gender_tags as select}
+                            <li class="mx-1 my-2 text-center">
+                                <p 
+                                    on:click={() => {
+                                        gender = select.type
+                                    }}
+                                    class={`cursor-pointer text-xs  py-3 shadow-lg rounded block leading-normal text-white hover:bg-pb-800 ${gender === select.type ? "bg-pb-800" : "bg-gray-200"}`} >
+                                    <i class="fas fa-space-shuttle text-base mr-1"></i>  { select.emoji }
+                                </p>
+                            </li>
+                        {/each}
+                    </ul>
+                <div class="px-1  m-4">
+                </div>
+                    <h2>타이틀</h2>
+                    <ul class="grid grid-cols-2 gap-2">
+                        {#each titles as select}
+                            <li class="mx-1 my-2 text-center">
+                                <p 
+                                    on:click={() => {
+                                        title = select.title
+                                    }}
+                                    class={`cursor-pointer text-xs  py-3 shadow-lg rounded block leading-normal text-white hover:bg-pb-800 ${title === select.type ? "bg-pb-800" : "bg-gray-200"}`} >
+                                    { select.emoji }
+                                </p>
+                            </li>
+                        {/each}
+                    </ul>
+                </div>
+                <div class="px-1 m-4">
+                    <h2>동물</h2>
+                    <ul class="grid grid-cols-4 gap-2">
+                        {#each mbtis as mbti}
+                            <li class="mx-1 my-2 text-center" on:click={() => handleClickMBTI(mbti)}>
+                                <p 
+                                    class={`cursor-pointer text-xs py-3 shadow-lg rounded block leading-normal text-white  bg-white-500 bg-gray-200 hover:bg-pb-800`} >
+                                    { mbti.emoji }
+                                </p>
+                            </li>
+                        {/each}
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
-</div>
+</IntersectionObserver>
